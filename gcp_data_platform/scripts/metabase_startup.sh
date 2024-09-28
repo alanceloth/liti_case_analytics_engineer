2ed91021d4aadfe7930fa9ffd052320eddab03e7
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-09-28 14:50:39 -0300

    fix(terraform): adicioinado Briefer

--- gcp_data_platform/scripts/metabase_startup.sh
+++ gcp_data_platform/scripts/metabase_startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Atualizar pacotes e instalar dependências
 sudo apt-get update
 sudo apt-get install -y curl git python3-pip docker.io docker-compose nginx postgresql
 
@@ -19,17 +21,27 @@ export MB_DB_HOST=localhost
 sudo systemctl start docker
 sudo systemctl enable docker
 
-
 # Configurar o Nginx como proxy reverso
 
-# Criar arquivo de configuração do Nginx para o Airflow
-sudo tee /etc/nginx/sites-available/airflow <<EOF
+# Criar arquivo de configuração do Nginx para o Metabase e Briefer
+sudo tee /etc/nginx/sites-available/analytics <<EOF
 server {
     listen 80;
     server_name _;
 
-    location / {
-        proxy_pass http://localhost:3000;
+    # Proxy para o Metabase (porta 3000)
+    location /metabase/ {
+        proxy_pass http://localhost:3000/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_redirect off;
+    }
+
+    # Proxy para o Briefer (porta 3001)
+    location /briefer/ {
+        proxy_pass http://localhost:3001/;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -42,8 +54,8 @@ EOF
 # Remover o link simbólico padrão do Nginx
 sudo rm /etc/nginx/sites-enabled/default
 
-# Ativar a nova configuração do Airflow
-sudo ln -s /etc/nginx/sites-available/airflow /etc/nginx/sites-enabled/
+# Ativar a nova configuração do Nginx
+sudo ln -s /etc/nginx/sites-available/analytics /etc/nginx/sites-enabled/
 
 # Testar a configuração do Nginx
 sudo nginx -t
@@ -51,17 +63,35 @@ sudo nginx -t
 # Reiniciar o Nginx para aplicar as alterações
 sudo systemctl restart nginx
 
+# Adicionar hostnames amigáveis ao arquivo /etc/hosts localmente
+sudo tee -a /etc/hosts <<EOF
+127.0.0.1 metabase.liti
+127.0.0.1 briefer.liti
+EOF
+
+# Esperar um tempo para garantir que os serviços sejam iniciados corretamente
 sleep 10
 
-#Metabase
-sudo docker run --restart unless-stopped -d -p 3000:3000 -e MB_DB_TYPE -e MB_DB_DBNAME -e MB_DB_PORT -e MB_DB_USER -e MB_DB_PASS -e MB_DB_HOST metabase/metabase
+# Iniciar Metabase
+sudo docker run --restart unless-stopped -d \
+  -p 3000:3000 \
+  -e MB_DB_TYPE \
+  -e MB_DB_DBNAME \
+  -e MB_DB_PORT \
+  -e MB_DB_USER \
+  -e MB_DB_PASS \
+  -e MB_DB_HOST \
+  metabase/metabase
 
-#Briefer
+# Iniciar Briefer
 sudo docker run --restart unless-stopped -d \
   -p 3001:3000 \
+  --env ALLOW_HTTP="true" \
   -v briefer_psql_data:/var/lib/postgresql/data \
   -v briefer_jupyter_data:/home/jupyteruser \
   -v briefer_briefer_data:/home/briefer \
   briefercloud/briefer
 
+
+# Enviar notificação via Google Cloud Pub/Sub sobre o status da VM
 gcloud pubsub topics publish vm-status-topic --message="VM $(hostname) pronta para uso às $(date)"
